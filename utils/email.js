dc4721069451a09ec8a59ac045de05d98b95044c
Marco Duhaney <marco.duhaney2023@gmail.com>
2024-03-19 17:01:48 -0500

    had to leave cz of mr ricky

--- utils/email.js
+++ utils/email.js
@@ -6,4 +6,51 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta. url));
 
-export
+export class Email {
+    #templateURL = path.join(__dirname,'../views/email/');
+    constructor(patient){
+        this.to = patient.email;
+        this.first_name = patient.first_name;
+        this.last_name = patient.last_name;
+        this.from = `[Mandeville Public/Hargreeves Memorial Hospital] <${process.env.EMAIL_FROM}>` 
+        // how does this know where the config file is?
+    }; 
+    createMailTranport(){
+        if(process.env.NODE_ENV != 'production'){
+            return nodemailer.createTransport({
+                host: 'sandbox.smtp.mailtrap.io',
+                port: 587,
+                auth: {
+                    user:process.env.MAILTRAP_USER,
+                    pass:process.env.MAILTRAP_PASS
+                }
+            });
+        }else{
+            return nodemailer.createTransport({
+                host: 'mail.CanteenDomain.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user:process.env.EMAIL_USER,
+                    pass:process.env.EMAIL_PASS
+                }
+            });
+        }
+    }
+
+    async sendMail(template, subject, Orderinfo){
+        const transport = this.createMailTranport();
+        const html = await ejs.renderFile(this.#templateURL + template + '.ejs', {
+            subject:subject,
+            patient_first_name: this.first_name,
+            patient_last_name: this.last_name,
+        });
+        return await transport.sendMail({
+            to:`${this.to}, ${process.env.COPY_EMAIL}`,
+            from:this.from,
+            html:html,
+            text:htmlToText(html),
+            subject:subject
+        });
+    }
+};
